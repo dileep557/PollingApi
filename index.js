33c388d336b9c4acf01006fcf11bdd0da67d7392
dileep <yadavdileep557@gmail.com>
2023-09-13 15:32:49 +0530

    first commit : Setup server

---
+++ index.js
@@ -0,0 +1,15 @@
+const Port=3000
+const express=require('express')
+const bodyParser=require('body-parser')
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}))
+const db=require('./config/mongoose');
+
+
+   
+app.listen(Port,function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log("server is runing ...",Port);
+})
